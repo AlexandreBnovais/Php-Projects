5117426f93415b0923171aabf007604406e2a4e5
AlexandreBnovais <157971881+AlexandreBnovais@users.noreply.github.com>
2025-05-31 22:27:05 -0300

    Delete account directory

--- account/account.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    if(session_status() !== PHP_SESSION_ACTIVE) {
-        session_start();
-    }
-
-    echo $_SESSION['user'] . "<br>";
-    echo $_SESSION['email'] . "<br>";
-    echo $_SESSION['password'] . "<br>";
-?>
